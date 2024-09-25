a16085102989c9ebee4dca968cc9a1ea03b650c5
IkeFeng <2063581814@qq.com>
2024-09-25 14:09:12 +0800

    新增Standard库中Socket客户端,服务端相关操作类

--- Ike.Standard/Func/Extensions.cs
+++ Ike.Standard/Func/Extensions.cs
@@ -3,13 +3,33 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 
-namespace Ike.Standard.Func
+namespace Ike.Standard
 {
 	/// <summary>
 	/// 扩展方法类
 	/// </summary>
 	public static class Extensions
 	{
+
+
+		
+
+
+		/// <summary>
+		///     检查值是否在<paramref name="minValue"/>和<paramref name="maxValue"/>之间(不包括<paramref name="minValue"/>)
+		/// </summary>
+		/// <param name="this">this</param>
+		/// <param name="minValue">时间段起始值</param>
+		/// <param name="maxValue">时间段结束值</param>
+		/// <returns>如果值介于<paramref name="minValue"/>和<paramref name="maxValue"/>之间，则为<see langword="true"></see>，否则为<see langword="false"></see></returns>
+		/// <remarks>
+		/// [<see langword="2024年9月24日11:02:32" />]
+		/// </remarks>
+		public static bool Between(this DateTime @this, DateTime minValue, DateTime maxValue)
+		{
+			return minValue.CompareTo(@this) == -1 && @this.CompareTo(maxValue) == -1;
+		}
+
 		/// <summary>
 		/// 安全地获取 <see cref="IEnumerable{T}"/> 中的元素数量，如果为 <see langword="null"/> 则返回 <see langword="0"/>
 		/// </summary>
